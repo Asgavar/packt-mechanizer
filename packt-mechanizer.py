d125930bb1a8b2751612ec2b99f03f159267cd37
Artur Juraszek <asgavar@gmail.com>
2016-05-21 15:53:58 +0200

    code improvements

--- packt-mechanizer.py
+++ packt-mechanizer.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python2
+
 import mechanize
 import json
+
 
 def get_login_info():
     with open("account.json") as account_json:
@@ -9,28 +12,22 @@ def get_login_info():
     password = account["password"]
     return email, password
 
+
 def login(browser, email, password):
     browser.form = list(browser.forms())[1]    # packt-user-login-form
-    # for control in browser.form.controls:
-    #     print control
     email_control = browser.form.find_control("email")
     password_control = browser.form.find_control("password")
     email_control.value = email
     password_control.value = password
-    # print email_control.value
-    # print password_control.value
     browser.submit()
 
+
 def claim(browser):
-    # for link in browser.links():
-    #     print link.url
     for link in browser.links():
         if "claim" in link.url:
             claim_link = link
             break
-    # print claim_link.url
     res = browser.follow_link(claim_link)
-    # print res.read()
 
 
 def main():
@@ -40,6 +37,7 @@ def main():
     br.open(url)
     login(br, email, password)
     claim(br)
+
 
 if __name__ == "__main__":
     main()
